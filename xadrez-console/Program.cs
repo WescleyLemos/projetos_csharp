c19e14573b5883bda76e58a9fe27c4cdb67e9b44
Wescley <wescley188@gmail.com>
2021-08-14 08:58:21 -0300

    Controlando as peças capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,10 +16,9 @@ namespace xadrez_console
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno:" + partida.turno);
-                        Console.WriteLine("Aguardando Jogada:" + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
+
+      
 
                         Console.Write("Origem:");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
